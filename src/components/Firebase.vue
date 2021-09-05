31820ba842fa0f578b7b96798f358ce335729815
Lukas Hermann <hey@lukashermann.dev>
2021-09-05 20:59:52 +0200

    firebase v8 setup with firestore

---
+++ src/components/Firebase.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h2>Firebase</h2>
+    <div>
+      {{ docs }}
+    </div>
+  </section>
+</template>
+
+<script>
+import FirebaseSingleton from '../firebase.js'
+
+export default {
+  data: () => ({
+    docs: [],
+  }),
+  async mounted () {
+    await FirebaseSingleton.initialize()
+    const firestore = await FirebaseSingleton.getFirestore()
+    const querySnapshot = await firestore.collection('users').get()
+    this.docs = querySnapshot.docs
+  },
+}
+</script>
